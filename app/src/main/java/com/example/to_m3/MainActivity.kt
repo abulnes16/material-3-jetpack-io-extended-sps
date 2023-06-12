2d2c045eb170ef7d4b8448933d4ecb9b951261f5
Angel Bulnes <angel_bulnes16@yahoo.com>
2023-06-11 19:04:33 -0600

    Add app dependencies

--- app/src/main/java/com/example/to_m3/MainActivity.kt
+++ app/src/main/java/com/example/to_m3/MainActivity.kt
@@ -3,12 +3,18 @@ package com.example.to_m3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.to_m3.ui.theme.ToM3Theme
 
@@ -31,10 +37,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(
+            painter = painterResource(id = R.drawable.jetpack_compose_icon),
+            contentDescription = null
+        )
+        Text(
+            text = "Hello $name!",
+            style = MaterialTheme.typography.displayLarge
+        )
+
+    }
+
 }
 
 @Preview(showBackground = true)
